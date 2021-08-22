42250d8393d40cbd333103318fb3895d50108b37
Alex Srebernic <alexsrebernic@gmail.com>
2021-08-22 18:09:06 -0300

    add function makeTable works

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-const container = document.querySelector("container");
-function makeDivs (number){
-    const div = document.createElement("div")
-    for(let i = 1 ; l <= number ; l++){
+const drawTable = document.querySelector(".drawTable");
 
+// Make the divs in the container function
+function makeTable (number){
+    for(let i = 1 ; i <= number*number ; i++){
+        const div = document.createElement("div")
+        drawTable.appendChild(div)
     }
+    drawTable.style.gridTemplateColumns = "repeat("+ number +",1fr)"
+    drawTable.style.gridTemplateRows = "repeat("+ number +",1fr)"
 }
+console.log(makeTable(8))
