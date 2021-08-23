da60434641b28f2e8eb07560be6c0966de872d68
Alex Srebernic <alexsrebernic@gmail.com>
2021-08-23 16:59:47 -0300

    add new css style , add paint function , add footer

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 const drawTable = document.querySelector(".drawTable");
-// Make the boxs in the container function
+// Make the boxs in the container 
 function makeTable (number){
     for(let i = 1 ; i <= number*number ; i++){
         const div = document.createElement("div")
-        div.className = "box"
+        div.id = "box"
+        div.setAttribute("draggable", "false");
+        div.setAttribute("data-bottom", "true");
+
         drawTable.appendChild(div)
     }
     drawTable.style.gridTemplateColumns = "repeat("+ number +",1fr)"
     drawTable.style.gridTemplateRows = "repeat("+ number +",1fr)"
 }
 makeTable(16)
-
 // Paint the boxs in the table
-const box = document.getElementsByClassName("box")
+const box = document.querySelectorAll("#box")
 function paintBox(){
     this.style.backgroundColor = "black"
 }
-box.addEventListener("click",paintBox)
+for(let l = 1 ; l < box.length ; l++){
+    box[l].addEventListener("mouseover",paintBox)
+}
